850da641363226eaa21462f060fc3abc01909020
kzndotsh <admin@kaizen.wtf>
2025-04-04 18:18:33 -0400

    feat(cloudflare): add new environment variables and R2 bucket support
    Introduce new environment variables `NEXTJS_ENV`,
    `NEXT_INC_CACHE_R2_BUCKET`, and `WORKER_SELF_REFERENCE` to enhance
    configuration flexibility. Integrate R2 incremental cache for Next.js
    by adding `r2IncrementalCache` to the Cloudflare configuration. Update
    scripts in `package.json` to streamline deployment and cache
    population processes. Add service bindings and R2 bucket definitions
    in `wrangler.toml` for better resource management and self-referencing
    capabilities. These changes aim to improve deployment efficiency,
    resource management, and caching capabilities for the application.

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,5 +1,6 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare';
+import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache';
 
 export default defineCloudflareConfig({
-  // TBD
+  incrementalCache: r2IncrementalCache,
 });
