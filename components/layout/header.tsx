fe325f9c5cb1b59587c15c941778184123219894
Logan Honeycutt <admin@kzn.sh>
2026-01-03 15:11:38 -0500

    fix: update header navigation item from 'Finance' to 'Open' for improved clarity

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -22,7 +22,7 @@ const navItems = [
   { name: 'Blog', href: '/blog' },
   { name: 'Wiki', href: 'https://atl.wiki' },
   { name: 'Tools', href: 'https://atl.tools' },
-  { name: 'Finance', href: '/finance' },
+  { name: 'Open', href: '/open' },
 ];
 
 // Logo component
